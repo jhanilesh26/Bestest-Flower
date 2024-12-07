3af401e7543dbdc96fb4f12957d69319bd9dd575
Nilesh Jha <100159953+jhanilesh26@users.noreply.github.com>
2024-12-08 00:15:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+onload = () =>{
+    document.body.classList.remove("container");
+};
+// Show the message after a delay
+setTimeout(function() {
+    document.getElementById('message').style.display = 'block';
+}, 2000); // Adjust the time as needed (5000ms = 5 seconds)
